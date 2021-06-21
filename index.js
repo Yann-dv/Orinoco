326718afaf88b4c4c0fa61f47b016306fc76713c
Yann Dev <h.yann_pro@yahoo.fr>
2021-06-21 15:33:02 +0200

    Add index.js, work on creatin dynamic teddy's cards, filter searchbar

---
+++ index.js
@@ -0,0 +1,35 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM entièrement chargé et analysé");
+
+    const addArticles = index => {
+      for(let i = 0; i < index; i++) {
+      const item = document.createElement('div')
+      item.setAttribute('class', 'teddy col-12 col-lg-4')
+      //InsertAdjacentHTML -> cartes des différents Teddy's
+      mainContainer.insertAdjacentHTML('afterbegin', '<div class="col-12 col-lg-4"><div class="card mb-4 mt-3 mt-lg-3 mb-lg-4 shadow"><img src="./images/teddy_1.jpg" alt="Teddy" class="card-img-top"><div class="card-body"><h5 class="card-title">Teddy 1</h5><p class="card-text">Premier teddy</p><a class="btn btn-primary stretched-link" href="lessons-1.html" role="button">Lien vers le produit</a></div></div>')
+      }
+  }
+
+const mainContainer = document.getElementById('teddyList')
+   addArticles(5)
+
+   document.querySelector("#searchInput").addEventListener("keyup", function() {
+    event.preventDefault();     
+    var value = this.value().toLowerCase();
+    document.querySelector("#teddyList .col-12").addEventListener("filter", function() {
+       this.toggle(this.text().toLowerCase().indexOf(value) > -1)
+    });
+ });
+
+  });
+
+    
+
+   /* $(document).ready(function(){
+      $("#searchInput").on("keyup", function() {
+         var value = $(this).val().toLowerCase();
+         $("#lessonList .col-12").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+         });
+      });
+   });*/
