48b856b3836aae95b01c471384ec4a075671fc31
Yann <79877110+Yann-dv@users.noreply.github.com>
2023-10-09 12:09:03 +0200

    fix: github url config string

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 let apiUrl = (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "")
 ? "http://localhost:3000"
-: "https://yann-dv.github.io/Orinoco/";
+: "https://yann-dv.github.io/Orinoco";
